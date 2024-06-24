6cfd56078156685176761c8e8e996e29045079c4
Sebastián Sanhueza <s.sanhueza1001@gmail.com>
2024-06-24 10:24:05 -0400

    a

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from app import create_app
 from config import ProductionConfig
 
+app = create_app(ProductionConfig)
+
 if __name__ == '__main__':
-    app = create_app(ProductionConfig)
     app.run()
